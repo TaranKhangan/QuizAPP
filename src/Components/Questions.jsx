fb081af00f13406c8e3492ff59776e343677d76e
Taran <khangantaran@gmail.com>
2025-04-05 11:40:30 +0530

    committed everything

---
+++ src/Components/Questions.jsx
@@ -0,0 +1,14 @@
+/*import React from "react";
+
+function Questions(){
+   const [showResult, setShowResult] = useState(false);
+
+    return(
+        <div>
+            <p className="heading-txt">Quiz Application</p>
+            <div className="container">
+
+            </div>
+        </div>
+    )
+}*/
