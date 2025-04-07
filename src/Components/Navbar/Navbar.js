31512408709177fe77a773de463de64ef3990cef
Taran <khangantaran@gmail.com>
2025-04-08 02:06:55 +0530

    Created navbar'content + created assests folder

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,8 +3,22 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='nav-container'>
+      <img src='' alt='' className='nav-logo'/>
+        <div className='nav-links'>
+        <ul>
+            <li>
+            <a href='#home'>Home</a></li>
+            <li><a href='#about'>About Us</a></li>
+            <li><a href='#services'>services</a></li>
+            <li><a href='#contact'>Contact</a></li>
+        </ul>
+        <img src='' alt=''/>
+        <div className='nav-btns'> 
+            <button className='login-btn'>Login</button>
+            <button className='signup-btn'>Sign up</button>
+        </div>
+        </div>
     </div>
   )
 }
