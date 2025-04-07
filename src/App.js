2c29f12557c7091ab793c98992a9124ca0447dbf
Taran <khangantaran@gmail.com>
2025-04-08 00:38:15 +0530

    Added the content in the app.js and removed everything else.. except Home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 //import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home';
+import React from 'react';
+const App =()=>{
+  return(
+    <div className='app-container'>
+      <Home/>
+    </div>
+  )
+}
+
+
+
+export default App;
+
+
+
+
+/*
 import Quiz from './Components/Quiz';
 import Result from './Components/Result';
 import React, { useState } from 'react';
@@ -7,22 +25,33 @@ import React, { useState } from 'react';
 const App = ()=>{
   const [score, setScore] = useState(0);
   const [isQuizOver, setIsQuizOver] = useState(false);
+  //////////////
+  const [isQuizStarted, setIsQuizStarted] = useState(false);
+////////////
 
   const handleQuizComplete = (finalScore) => {
     setScore(finalScore);
     setIsQuizOver(true);
   };
-
-  return (
+/++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++/
+  return ({/* 
     <div className="app-container">
-      <h1>Quiz App</h1>
-      {isQuizOver ? (
+      show Home if quiz hasn't started...
+      {!isQuizStarted?(
+        <Home onStartQuiz={startQuiz}/>
+      ):(
+        <>
+        {isQuizOver ? (
        <Result score={score} total={2} />
       ) : (
         <Quiz onQuizComplete={handleQuizComplete} />
       )}
+        </>
+      )}
+      <h1>Quiz App</h1>
+      
     </div>
   );
 }
+*/
 
-export default App;
