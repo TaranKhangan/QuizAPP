ce7b86a77c11e7bc2501cdd26a36490ba1ec8a92
Taran <khangantaran@gmail.com>
2025-04-08 17:00:30 +0530

    added images and messed it up

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Navbar.css';
+import refLogo from '../../assets/ref-Logo.jpeg';
+import toogle_Light from '../../assets/toogle_Light.png';
+
 
 const Navbar = () => {
   return (
-    <div className='nav-container'>
+    <div className='nav-container navbar'>
       <img src='' alt='' className='nav-logo'/>
         <div className='nav-links'>
         <ul>
@@ -13,7 +16,9 @@ const Navbar = () => {
             <li><a href='#services'>services</a></li>
             <li><a href='#contact'>Contact</a></li>
         </ul>
-        <img src='' alt=''/>
+
+        <img src= '' alt='toogle' className='toogle-icon'/>
+
         <div className='nav-btns'> 
             <button className='login-btn'>Login</button>
             <button className='signup-btn'>Sign up</button>
