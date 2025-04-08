07095b6c1ecdb3a6a4ae70b55f8312f3b5c8cdb3
Taran <khangantaran@gmail.com>
2025-04-09 01:09:53 +0530

    Started making home page better

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,8 +4,8 @@ import './Home.css';
 const Home = () => {
   return (
     <div className='container'>
-        <div className='logo'>Sahash</div>
-        <h1>Sahash</h1>
+        <div className='logo'>S</div>
+        <h1 className='logo-Name'>Sahash</h1>
         <h4><i>We Care All</i></h4>
         <div className='buttons'>
             <button className='login-btn'>Login</button>
