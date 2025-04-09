e761f07311c3762f6945b1a603870a776836b79e
Taran <khangantaran@gmail.com>
2025-04-09 22:35:08 +0530

    Made changes in the styling

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,8 +4,10 @@ import './Home.css';
 const Home = () => {
   return (
     <div className='container'>
+      <div className='logo-container'>
         <div className='logo'>S</div>
         <h1 className='logo-Name'>Sahash</h1>
+      </div>
         <h4><i>We Care All</i></h4>
         <div className='buttons'>
             <button className='login-btn'>Login</button>
