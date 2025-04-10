e0a8e2bc35055361588fc9c4601ec61ac6ba52ec
Taran <khangantaran@gmail.com>
2025-04-10 17:33:19 +0530

    Aligned the navbar's items and styled the home.css

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -33,7 +33,7 @@ const Navbar = () => {
           <li>
             <a href='#home'>Home</a></li>
           <li><a href='#about'>About Us</a></li>
-          <li><a href='#services'>services</a></li>
+          <li><a href='#services'>Services</a></li>
           <li><a href='#contact'>Contact Us</a></li>
         </ul>
       </div>
