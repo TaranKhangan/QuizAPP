4faf1bcdcf7534fa3d64a92e4d3b120f1c6be940
Taran <khangantaran@gmail.com>
2025-04-12 02:36:11 +0530

    added an image

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 import './Home.css';
+import BookHeader from '../../assets/Book-Header.png';
 
 const Home = () => {
+  const [showButtons, setShowButtons] = useState(window.innerWidth<=768);
+ 
+  useEffect(()=>{
+    let mounted = true;
+    const handleResize = () => {
+      if(mounted){
+      setShowButtons(window.innerWidth<=768);
+    }
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      mounted = false;
+      window.removeEventListener('resize', handleResize);
+    }
+  })
   return (
     <div className='container'>
+      <div className='content-wrapper'>
+        <div className='text-content'>
       <div className='logo-container'>
         <div className='logo'>S</div>
         <h1 className='logo-Name'>Sahash</h1>
       </div>
-        <h4><i>We Care All</i></h4>
+        <h4><i>Every Answer Counts, So Does Every Life.</i></h4><h4>
+        <i>Where Knowledge Meets Kindness.</i></h4><h4>
+        <i>Be the Brain Behind the Change.</i></h4>
+        {showButtons &&(
         <div className='buttons'>
             <button className='login-btn'>Login</button>
             <button className='signup-btn'>Sign up</button>
         </div>
+        )}
+        </div>
+        <div className="image-content">
+          <img src={BookHeader} alt="Book Header" className="book-header" />
+        </div>
+    </div>
     </div>
   );
 }
